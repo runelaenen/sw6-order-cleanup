b6146a8ed5993ab5de9753d2ac8bb0de82e50a33
runelaenen <rune@laenen.me>
2026-06-11 13:45:01 +0200

    6.7 compatibility

--- src/Api/Controller/OrderCleanupController.php
+++ src/Api/Controller/OrderCleanupController.php
@@ -6,7 +6,7 @@ use OrderCleanup\Service\OrderCleanupService;
 use Shopware\Core\Framework\Context;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route(defaults: ['_routeScope' => ['api']])]
 class OrderCleanupController extends AbstractController
